d545ab5c1b9fb6620ee87668666c9454d3eb8bc9
Apurva Anand <apurvaanand51@gmail.com>
2026-01-30 22:35:31 +0530

    uploade code for reverse integer.c

---
+++ 7-reverse integer.c
@@ -0,0 +1,17 @@
+#include <limits.h>
+int reverse(int x){
+    int rev = 0, temp = x, rem = 0;
+
+        while(temp != 0){
+            rem = temp % 10;
+            temp = temp / 10;
+
+            if ((rev > INT_MAX / 10 || (rev == INT_MAX / 10 && rem > 7)) || (rev < INT_MIN / 10 || (rev ==   INT_MIN / 10 && rem < -8))) {
+                return 0;
+            }
+
+            rev = rev * 10 + rem;
+        }
+
+    return rev;
+}
