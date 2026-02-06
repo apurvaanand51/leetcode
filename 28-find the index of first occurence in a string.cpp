f1db0813fcbb3674c4daaedce48ecbb6f1533696
Apurva Anand <apurvaanand51@gmail.com>
2026-02-06 22:23:41 +0530

    uploaded code for find the index of first occurence in a string.cpp

---
+++ 28-find the index of first occurence in a string.cpp
@@ -0,0 +1,11 @@
+#include <string>
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        size_t found = haystack.find(needle);
+        if (found != string::npos) {
+            return static_cast<int>(found);
+        }
+        return -1;
+    }
+};
